20776447ef9d0b1141d5a2cbcbdad4530653c4f5
linghuchong <992049491@qq.com>
2019-04-27 19:15:54 +0800

    独孤九剑第一版

---
+++ src/main/java/c/a/dugujiujian/Dugujiujian.java
@@ -0,0 +1,16 @@
+package c.a.dugujiujian;
+
+/**
+ * @author ???
+ * 2019-04-27 19:09
+ */
+public class Dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("我是独孤九剑/");
+        System.out.println("有我天下无毒");
+        System.out.println("有我东邪西毒");
+        System.out.println("有我西电东蜀");
+        System.out.println("有我南辕北辙");
+        System.out.println("有我南水北调");
+    }
+}
